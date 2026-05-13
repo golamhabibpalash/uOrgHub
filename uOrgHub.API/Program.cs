67d790bf8c7a7b893c6a720be1e738a8d2bbd0c3
golamhabibpalash <golamhabibpalash@gmail.com>
2026-05-13 06:24:48 +0600

    Update project structure with API and controllers

--- uOrgHub.API/Program.cs
+++ uOrgHub.API/Program.cs
@@ -1,14 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using uOrgHub.Shared.Data;
-using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Serilog
-builder.Host.UseSerilog((ctx, lc) => lc
-    .ReadFrom.Configuration(ctx.Configuration)
-    .WriteTo.Console()
-    .WriteTo.File("logs/orgHub-.log", rollingInterval: RollingInterval.Day));
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,33 +9,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 // Controllers
 builder.Services.AddControllers();
-
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Auto-migrate on startup (optional - won't crash if DB unavailable)
-try
+// Auto-migrate on startup
+using (var scope = app.Services.CreateScope())
 {
-    using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
-catch (Exception ex)
-{
-    Console.WriteLine($"Database migration skipped: {ex.Message}");
-    Console.WriteLine("The application will continue without applying migrations.");
-}
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+Console.WriteLine("=== uOrgHub API STARTED ===");
+
 app.Run();
