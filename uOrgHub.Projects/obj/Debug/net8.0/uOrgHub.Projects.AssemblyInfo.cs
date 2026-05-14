124dbf8020d71893db325bcd54f79f2a3076fd6e
abdulhamidrana <rana.eng.2012@gmail.com>
2026-05-14 10:09:19 +0600

    test commit

--- uOrgHub.Projects/obj/Debug/net8.0/uOrgHub.Projects.AssemblyInfo.cs
+++ uOrgHub.Projects/obj/Debug/net8.0/uOrgHub.Projects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uOrgHub.Projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08ac854326035701e2905adb88baff1a59ee63a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5044e721ec0fb7000b0bcc0d9dcc9ee2cbf85b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("uOrgHub.Projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uOrgHub.Projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
