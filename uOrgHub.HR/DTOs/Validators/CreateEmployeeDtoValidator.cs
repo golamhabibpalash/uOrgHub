7f6823c7fe3288b453f7399722079f1904e90f05
golamhabibpalash <golamhabibpalash@gmail.com>
2026-06-06 15:23:28 +0600

    email and phone number validation added

--- uOrgHub.HR/DTOs/Validators/CreateEmployeeDtoValidator.cs
+++ uOrgHub.HR/DTOs/Validators/CreateEmployeeDtoValidator.cs
@@ -10,11 +10,13 @@ public class CreateEmployeeDtoValidator : AbstractValidator<CreateEmployeeDto>
         RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
         RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
         RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(200);
+        RuleFor(x => x.PersonalEmail).EmailAddress().MaximumLength(200).When(x => !string.IsNullOrWhiteSpace(x.PersonalEmail));
+        RuleFor(x => x.Phone).MaximumLength(20).Matches(@"^\+?[\d\s\-\(\)]{7,20}$").When(x => !string.IsNullOrWhiteSpace(x.Phone));
+        RuleFor(x => x.MobilePhone).MaximumLength(20).Matches(@"^\+?[\d\s\-\(\)]{7,20}$").When(x => !string.IsNullOrWhiteSpace(x.MobilePhone));
         RuleFor(x => x.DesignationId).NotEmpty();
         RuleFor(x => x.DepartmentId).NotEmpty();
         RuleFor(x => x.JoiningDate).NotEmpty();
         RuleFor(x => x.BasicSalary).GreaterThanOrEqualTo(0);
         RuleFor(x => x.NationalId).MaximumLength(30).When(x => x.NationalId != null);
-        RuleFor(x => x.Phone).MaximumLength(20).When(x => x.Phone != null);
     }
 }
--- uOrgHub.HR/DTOs/Validators/UpdateEmployeeDtoValidator.cs
+++ uOrgHub.HR/DTOs/Validators/UpdateEmployeeDtoValidator.cs
@@ -9,6 +9,9 @@ public class UpdateEmployeeDtoValidator : AbstractValidator<UpdateEmployeeDto>
         RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
         RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
         RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(200);
+        RuleFor(x => x.PersonalEmail).EmailAddress().MaximumLength(200).When(x => !string.IsNullOrWhiteSpace(x.PersonalEmail));
+        RuleFor(x => x.Phone).MaximumLength(20).Matches(@"^\+?[\d\s\-\(\)]{7,20}$").When(x => !string.IsNullOrWhiteSpace(x.Phone));
+        RuleFor(x => x.MobilePhone).MaximumLength(20).Matches(@"^\+?[\d\s\-\(\)]{7,20}$").When(x => !string.IsNullOrWhiteSpace(x.MobilePhone));
         RuleFor(x => x.DesignationId).NotEmpty();
         RuleFor(x => x.DepartmentId).NotEmpty();
         RuleFor(x => x.BasicSalary).GreaterThanOrEqualTo(0);
