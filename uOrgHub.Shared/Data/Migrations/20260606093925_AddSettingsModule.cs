a690ee13cac919b52b08a6876c13308480cf4819
golamhabibpalash <golamhabibpalash@gmail.com>
2026-06-06 15:54:45 +0600

    migration: add SettingsModule migration for sett_settings and sett_validation_rules tables

---
+++ uOrgHub.Shared/Data/Migrations/20260606093925_AddSettingsModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace uOrgHub.Shared.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSettingsModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
